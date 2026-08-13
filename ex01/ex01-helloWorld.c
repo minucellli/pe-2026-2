7c95311abf820afe25b70d9d2132b9925d5ce698
LetíciaCM <160792986+minucellli@users.noreply.github.com>
2026-08-13 11:54:51 +0000

    exercícios

--- ex01/ex01-helloWorld.c
+++ ex01/ex01-helloWorld.c
@@ -1,5 +1,29 @@
-#include <stdio.h>
+/*#include <stdio.h>
+
 int main() {
-    printf("Olá, turma!\n");
+    int idade; 
+    do {
+        printf("Digite a sua idade: ");
+        scanf("%d", &idade);
+    } while(idade < 0 || idade > 130);
+    printf("Você tem %d anos e nasceu em %d\n", idade, 2026 - idade);
     return 0;
 }
+*/
+
+/*#include <stdio.h>
+
+#define IDADE_MINIMA 0
+#define IDADE_MAXIMA 130
+#define ANO_ATUAL 2025
+
+int main() {
+    int idade;
+    do {
+        printf("Digite a sua idade: ");
+        scanf("%d", &idade);
+    } while (idade < IDADE_MINIMA || idade > IDADE_MAXIMA);
+    printf("Você tem %d anos e nasceu em %d\n", idade, ANO_ATUAL - idade);
+    return 0;
+}
+*/
---
+++ ex02/ex-02.c
@@ -0,0 +1,25 @@
+/* #include <stdio.h>
+int main() {
+    for(int i = 1; i <= 20; i+= 1) {
+        printf("%d", i);
+        if (i < 20) {
+            printf(", ");
+        }
+    }
+    printf("\n");
+    return 0;
+} */
+
+/* #include <stdio.h>
+
+int main() {
+    for (int i = 2; i <= 20; i+=2) {
+        if (i % 2 == 0) {
+            printf("%d", i);
+            if (i < 20) {
+                printf(", ");
+            }
+        }
+    }
+    return 0;
+} */
